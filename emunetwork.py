5405c7e2a42a5b83b84d9945be5b2b10a1d9e588
Laura Scoccianti <laura.scoccianti@gmail.com>
2022-05-08 13:49:03 +0200

    simulazione base

--- emunetwork.py
+++ emunetwork.py
@@ -1,5 +1,84 @@
+from emurouter import Router
+import random
+import time
+
 # TO DO HERE
 # while(duration) --> emulate traffic flows going through the router.
 # traffic is defined as a "tuple": < router_ingress_intf ; router_egress_intf ; data_size >
 # what we want to achieve is that given the bandwidth provided by the router (which mainly depends on the slowest interface used by the traffic) and given the size of the data,
 # we are able to compute the time needed by the router to "process" that data, and therefore the interface utilization in terms of electric energy
+
+
+if __name__ == "__main__":
+    #duration = input("Choose the duration of the simulation (in seconds): ")
+    duration = 120
+
+    router = Router()
+
+    router_ports_bandwidth = router.get_ports_bw()
+    router_ports_ip = router.get_ports_ip()
+
+    # =======
+    # Print summary of router interfaces
+    # =======
+    print("Interface\tIP\t\t\tSpeed (Mb/s)")
+    for k in router_ports_bandwidth.keys():
+        if k in router_ports_ip.keys():
+            print(f"{k}\t\t{router_ports_ip[k]}\t\t{router_ports_bandwidth[k]}")
+    # =======
+
+    traffic_flows = {}
+
+    for i in range(10):
+        start_time = random.randint(0,duration)
+        while start_time in traffic_flows.keys():
+            start_time = random.randint(0,duration)
+        in_intf = "s01"
+        e_intf = "eth1"
+        data_size = random.randint(1,1200)
+        traffic_flows[str(start_time)] = [in_intf, e_intf, data_size]
+
+    print("Traffic flows summary\nTime\t\tIngress intf\tEgress intf\tSize (MB)")
+    for k,v in traffic_flows.items():
+        print(f"{k}\t\t{v[0]}\t\t{v[1]}\t\t{v[2]}")
+
+    # =======
+    # Basic simulation run
+    # =======
+    seconds = 0
+    while seconds<duration:
+        print(f"Time {seconds}")
+        s = str(seconds)
+        if len(router.buffer) != 0:
+            traffic = router.buffer.pop(0)
+            ingress_interface = traffic[0]
+            egress_interface = traffic[1]
+            traffic_size = traffic[2]
+            # fai tutto quello che fa sotto
+        if s in traffic_flows.keys():
+            ingress_interface = traffic_flows[s][0]
+            egress_interface = traffic_flows[s][1]
+            traffic_size = traffic_flows[s][2]
+            #required_time = router.traffic_time(ingress=ingress_interface, egress=egress_interface, data_size=traffic_size)
+            ingress_ticks = router.intf_time(interface=ingress_interface, data_size=traffic_size)
+            egress_ticks = router.intf_time(interface=egress_interface, data_size=traffic_size)
+            if router.port_status[ingress_interface] == 0:
+                router.set_port_status(interface=ingress_interface, status=ingress_ticks)
+                print(f"Router interface {ingress_interface} will now be busy for {ingress_ticks} seconds")
+                if router.port_status[egress_interface] == 0:
+                    router.set_port_status(interface=egress_interface, status=egress_ticks)
+                    print(f"Router interface {egress_interface} will now be busy for {egress_ticks} seconds")
+                else:
+                    traffic_flows[s][0] = 0 # il traffico è già NEL router, deve solo uscire... ma forse non è corretto ed è troppo complicato... mica il router si tiene dentro 1 GB di pacchetti!!
+                    (router.buffer).append(traffic_flows[s])
+            else:
+                (router.buffer).append(traffic_flows[s])
+                print(f"Traffic flow {traffic_flows[s]} has been added to the buffer")
+            print(f"Time required for traffic at time {seconds}: {required_time} seconds")
+        for k,v in router.port_status.items():
+            if v>0:
+                router.port_status[k] = v-1
+                
+        seconds += 1
+        time.sleep(0.5)
+    # =======
--- emurouter.py
+++ emurouter.py
@@ -6,10 +6,13 @@ class Router:
         self.ports_bw = {}
         self.ports = port_labels
         self.port_speed = port_speed
-        self.ports_bw = set_ports_bw(port_speed)
-        self.ports_ip = set_ports_ip(port_ip)
-        self.port_energy = init_port_energy()
-        self.routes = {}
+        self.ports_bw = self.set_ports_bw(port_speed)
+        self.ports_ip = self.set_ports_ip(port_ip)
+        self.port_energy = self.init_port_energy()
+        self.routes = {}    # mi sa che non serve
+        self.port_status = {}   # dictionary. Indicates for how many seconds the interface is still busy (e.g. "eth1": 0 means that it's free)
+        init_port_status()
+        self.buffer = []    # if an interface is already occupied, add the traffic to the buffer
 
     
     def set_ports_bw(self, port_speed):
@@ -24,6 +27,11 @@ class Router:
             ports_ip[self.ports[i]] = port_ip[i]
         return ports_ip
 
+    def init_port_status(self):
+        for i in range(len(self.ports)):
+            self.port_status[self.ports[i]] = 0
+        
+
     def init_port_energy(self):
         port_energy = {}
         for i in range(len(self.ports)):
@@ -35,16 +43,30 @@ class Router:
 
     def get_portspeed(self):
         return self.port_speed
+    
+    def get_ports_bw(self):
+        return self.ports_bw
+
+    def get_ports_ip(self):
+        return self.ports_ip
+
+    def set_port_status(self, interface, status):
+        self.port_status[interface] = status
 
     def add_route(self, interface, ip):
         self.routes[ip] = interface
 
     # defines how many ticks the router need to let a data stream pass through its interfaces
     def traffic_time(self, ingress, egress, data_size):
-        in_speed = self.ports_bw[ingress]
-        out_speed = self.ports_bw[egress]
+        in_speed = int(self.ports_bw[ingress])
+        out_speed = int(self.ports_bw[egress])
         speed = min(in_speed, out_speed)
         ticks = int(math.ceil(data_size / speed))   # always round up, so to more or less take into account the computational time... it balances through the whole simulation
+        return ticks
+
+    def intf_time(self, interface, data_size):
+        intf_speed = int(self.ports_bw[interface])
+        ticks = int(math.ceil(data_size/speed))
         return ticks
 
     def compute_energy(self, interface, time, speed):
